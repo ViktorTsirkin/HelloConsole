a49321bc96a0ef672e3af0eee31e921f236a6339
Viktor Tsirkin <reset1985@mail.ru>
2022-09-17 14:57:27 +0300

    add if-else

---
+++ 5_if-else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "виктор")
+{
+    Console.WriteLine("Ура, это же Витя!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
