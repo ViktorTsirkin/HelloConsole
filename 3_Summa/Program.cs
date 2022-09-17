be57374493dbd91dcd2374ef3a4288155e124497
Viktor Tsirkin <reset1985@mail.ru>
2022-09-17 14:41:25 +0300

    add Summa random number

--- 3_Summa/Program.cs
+++ 3_Summa/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 100); //числа от 1 дро 99
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100); //числа от 1 до 99
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
